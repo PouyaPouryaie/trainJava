a183814f2eaba54b233dc0a9280f6a90135b2401
pouyapouryaie <pouyapouryaie@gmail.com>
2021-10-10 16:44:42 +0330

    file base64 sample

---
+++ src/normaltrain/javacore/filesample/FileBase64Sample.java
@@ -0,0 +1,32 @@
+package normaltrain.javacore.filesample;
+
+import java.io.*;
+import java.util.Base64;
+
+public class FileBase64Sample {
+
+    public static void main(String[] args) {
+
+        //Read Source
+        File source = new File("../trainJava/white_sand-wallpaper-1920x1080.jpg");
+
+        //Write Source
+        String path = "./image.jpg";
+        byte[] bytes;
+        try {
+            FileInputStream in = new FileInputStream(source);
+            bytes = in.readAllBytes();
+            Base64.Encoder encoder =  Base64.getEncoder();
+            byte[] encode = encoder.encode(bytes);
+            Base64.Decoder decoder = Base64.getDecoder();
+            byte[] imageBytes = decoder.decode(encode);
+            File result =new File(path);
+            OutputStream outputStream = new FileOutputStream(result);
+            outputStream.write(imageBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("is done");
+    }
+}
