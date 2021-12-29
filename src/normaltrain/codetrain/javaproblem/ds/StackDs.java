f0d608f5f371ccbcbe89e62ec65742926b01e1f4
pouyapouryaie <pouyapouryaie@gmail.com>
2021-12-29 07:28:04 +0330

    JavaDataStructuresTest

---
+++ src/normaltrain/codetrain/javaproblem/ds/StackDs.java
@@ -0,0 +1,52 @@
+package normaltrain.codetrain.javaproblem.ds;
+
+import java.util.ListIterator;
+import java.util.Stack;
+
+public class StackDs {
+
+    public static void main(String[] args) {
+        Stack<Person> stack = new Stack<>();
+        stack.push(new Person("ali", 10));
+        stack.push(new Person("pouya", 20));
+        stack.push(new Person("atena", 30));
+
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        stack.forEach(System.out::println);
+        stack.stream().forEach(person -> System.out.println(person.getName()));
+        ListIterator<Person> personListIterator = stack.listIterator();
+        while (personListIterator.hasNext()){
+            System.out.println(personListIterator.next());
+        }
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+
+    }
+
+    static class Person{
+        private final String name;
+        private final Integer age;
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+}
