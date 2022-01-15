04972d0bfda8fe0bdf5cb4eb9bb3063dc30d787f
pouyapouryaie <pouyapouryaie@gmail.com>
2022-01-15 09:09:56 +0330

    Map use in stream

--- src/normaltrain/javacore/datastrutures/MapSample.java
+++ src/normaltrain/javacore/datastrutures/MapSample.java
@@ -1,7 +1,10 @@
 package normaltrain.javacore.datastrutures;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MapSample {
 
@@ -21,5 +24,23 @@ public class MapSample {
 
         map.merge(3, "val 3", (value, newValue) -> newValue);
         System.out.println("index " + 3 + " after merge is " + map.get(3));
+
+        Map<String, String> books = new HashMap<>();
+        books.put("978-0201633610", "Design patterns : elements of reusable object-oriented software");
+        books.put("978-1617291999", "Java 8 in Action: Lambdas, Streams, and functional-style programming");
+        books.put("978-0134685991", "Effective Java");
+
+        Optional<String> optionalIsbn = books.entrySet().stream()
+                .filter(e -> "Effective Java".equals(e.getValue()))
+                .map(Map.Entry::getKey)
+                .findFirst();
+        optionalIsbn.ifPresent(s -> System.out.println("Isbn is " + s));
+
+        List<String> collect = books.entrySet().stream()
+                .filter(e -> e.getKey().startsWith("978-0"))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+
+        collect.forEach(System.out::println);
     }
 }
