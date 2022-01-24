263cb36498a2ec0fe36d091cbf57aeef9c9640aa
pouyapouryaie <pouyapouryaie@gmail.com>
2022-01-24 07:56:19 +0330

    Optional and comparator:
    1 - how use comparator for sort in multi fields.
    2 - how use nullable and orElseGet of Optional class.

---
+++ src/normaltrain/javacore/comparator/ComparatorSample.java
@@ -0,0 +1,24 @@
+package normaltrain.javacore.comparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComparatorSample {
+
+    public static void main(String[] args) {
+        List<Human> humans = new ArrayList<>(){{
+            add(new Human("alex", 40));
+            add(new Human("pouya", 20));
+            add(new Human("ali", 20));
+            add(new Human("mehdi", 40));
+        }};
+
+        AgeCompare ageCompare = new AgeCompare();
+        NameCompare nameCompare = new NameCompare();
+
+        humans.sort(ageCompare.thenComparing(nameCompare));
+
+        humans.forEach(System.out::println);
+
+    }
+}
---
+++ src/normaltrain/javacore/comparator/Human.java
@@ -0,0 +1,28 @@
+package normaltrain.javacore.comparator;
+
+public class Human {
+
+    private final String name;
+    private final Integer age;
+
+    public Human(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
