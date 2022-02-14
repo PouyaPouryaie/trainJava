8061a8bc517ab8fb99dfb81b5f880018eee6f3ab
pouyapouryaie <pouyapouryaie@gmail.com>
2022-02-14 13:52:51 +0330

    Java String Tokens:
    how tokenize string with split and regex pattern

---
+++ src/normaltrain/codetrain/javaproblem/string/StringTokens.java
@@ -0,0 +1,24 @@
+package normaltrain.codetrain.javaproblem.string;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//He is a very very good boy, isn't he?
+public class StringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        scan.useDelimiter("\\Z");
+        String s = scan.next().trim();
+        if (s.length() > 0) {
+            String regex = "[ !,?._'@]+";
+            String[] split = s.split(regex);
+            System.out.println(split.length);
+            Arrays.stream(split).forEach(System.out::println);
+
+        } else {
+            System.out.println(0);
+        }
+        scan.close();
+    }
+}
