4d394438a347f39ba3146f3eefe147bb498e452d
pouya <pouyapouryaie@gmail.com>
2023-02-27 17:13:25 -0400

    leetcode train DFS

---
+++ src/normaltrain/codetrain/leetcode/algorithm1/FloodFill.java
@@ -0,0 +1,40 @@
+package normaltrain.codetrain.leetcode.algorithm1;
+
+/**
+ * DFS/BFS
+ * 733. Flood Fill
+ * An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+ * You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+ * To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+ * Return the modified image after performing the flood fill.
+ *
+ * Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+ * Output: [[2,2,2],[2,2,0],[2,0,1]]
+ * Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+ * Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+ */
+public class FloodFill {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) {
+            dfs(image, sr, sc, color, newColor);
+        }
+        return image;
+    }
+
+    public void dfs(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) {
+            return;
+        }
+        image[sr][sc] = newColor;
+        dfs(image, sr + 1, sc, color, newColor);
+        dfs(image, sr - 1, sc, color, newColor);
+        dfs(image, sr, sc + 1, color, newColor);
+        dfs(image, sr, sc - 1, color, newColor);
+    }
+}
+
